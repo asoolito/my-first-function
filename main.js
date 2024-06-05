8b6ce065f5c30cdcb2ad6d3564101e00959aa248
asoolito <roblox890king890@gmail.com>
2024-06-05 08:43:32 +0500

    my first function

---
+++ main.js
@@ -0,0 +1,41 @@
+// let  cars = ["matiz ","spark","malibu","nexia","lacetti","gentra"];
+
+// for (const chevrolet of cars) {
+//     console.log(chevrolet)
+// }
+
+// let cars = ["matiz ","spark","malibu","nexia","lacetti","gentra"]
+
+// cars.forEach(arrayniIchidagiElemantlar =>{
+//     console.log(arrayniIchidagiElemantlar);
+// })
+// let box = []
+// let box2 = [] 
+// let car = ["matiz ","spark","malibu","nexia","lacetti","gentra"];
+
+// car.forEach(arrayniIchidagiElemantlar =>{
+//     if(arrayniIchidagiElemantlar.includes("t")){
+//         box.push(arrayniIchidagiElemantlar);
+//     }
+//     else{
+//         box2.push(arrayniIchidagiElemantlar);
+//     }
+// })
+// console.log(box)
+// console.log(box2)
+
+let userNumber = +prompt ("birinchi sonni kiriting")
+let userNumber2 = +prompt("ikkinchi sonni kiriting")
+
+// if(userNumber<userNumber2) {
+//     let message = alert(`eng katta son ${userNumber2}`)
+// }else{
+//     let message = alert(`eng katta son ${userNumber}`)
+// }
+function add (a,b) {
+    if(a<b) {
+        let message = alert(`eng katta son ${b}`)
+    }else{
+        let message = alert(`eng katta son ${a}`)
+}}
+add(userNumber,userNumber2)
